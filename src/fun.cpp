b72f4fc4f0542deb57d6a66946c8deb115869f31
MichailImchikov <93055597+MichailImchikov@users.noreply.github.com>
2024-05-22 21:58:07 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,64 +1,107 @@
 // Copyright 2022 UNN-IASR
 #include <iostream>
+#include<vector>
 #include <cmath>
 #include <cctype>
-unsigned int faStr1(const char *str) {
-    bool InWord = false;
-    bool HaveANumbers = false;
-    int NumOfWords = 0;
+unsigned int faStr1(const char* str)
+{
+    bool newWord = false;
+    int Count = 0;
     int i = 0;
-    while (str[i]) {
-        if (!isblank(str[i])) {
-            if (!InWord)
-                InWord = true;
-            if (isdigit(str[i])) HaveANumbers = true;
-        } else {
-            if (InWord) {
-                if (!HaveANumbers) NumOfWords++;
-                InWord = false;
-                HaveANumbers = false;
+    std::vector<char> bufStr;
+    while (str[i])
+    {
+        if (newWord)
+        {
+            bool world = true;
+            for (int j = 0; j < bufStr.size(); j++)
+            {
+                if (!isalpha(bufStr[j]))
+                {
+                    world = false;
+                }
             }
+            if (world&&bufStr.size()>0)
+            {
+                Count++;
+            }
+            newWord = false;
+            bufStr.clear();
+            continue;
         }
+        if (str[i] == ' '||str[i]=='\0') {
+            newWord = true;
+            i++;
+            continue;
+        }
+        bufStr.push_back(str[i]);
         i++;
     }
-    if (InWord && !HaveANumbers) NumOfWords++;
-    return NumOfWords;
+    bool world = true;
+    for (int j = 0; j < bufStr.size(); j++)
+    {
+        if (!isalpha(bufStr[j]))
+        {
+            world = false;
+        }
+    }
+    if (world && bufStr.size() > 0)
+    {
+        Count++;
+    }
+    return Count;
 }
-
-unsigned int faStr2(const char *str) {
+unsigned int faStr2(const char* str) {
     bool flag = false;
+    float result = 0;
     int i = 0;
     int count = 0;
-    int result = 0;
+    bool IsWorld = true;
     while (str[i]) {
-        if (!flag && str[i] != ' ' && str[i] >= 65 && str[i] <= 90) {
+        if (!flag && str[i] != ' ' && str[i] >= 65 && str[i] <= 90&&IsWorld) {
             flag = true;
             count = 1;
-        } else if (flag && str[i] == ' ') {
+        }
+        else if (flag && str[i] == ' ') {
             flag = false;
             result += count;
-        } else if (str[i] < 97 || str[i] > 122) {
+            if (flag) {
+                IsWorld = true;
+            }
+        }
+        else if (str[i] < 97 || str[i] > 122) {
             count = 0;
         }
         i++;
+        if (flag) {
+            IsWorld = true;
+        }
     }
-    return result;
+    return (int)result;
 }
-
-unsigned int faStr3(const char *str) {
-    bool inWord = false;
-    int counter = 0;
+unsigned int faStr3(const char* str) {
     int i = 0;
-    int len = 0;
-    while (str[i] != '\0') {
-        if (str[i] != ' ') {
-            if (!inWord) counter++;
-            inWord = true;
-            len++;
-        } else if (str[i] == ' ' && inWord) {
-            inWord = false;
+    int SumChar = 0;
+    int CountWord=0;
+    int BufWord = 0;
+    while (str[i]) {
+        if (str[i] == ' ') {
+            if (BufWord > 0) {
+                CountWord++;
+            }
+            SumChar += BufWord;
+            BufWord = 0;
+        }
+        else {
+            BufWord++;
         }
         i++;
     }
-    return round(static_cast<float>(len)/counter);
+    if (str[i - 1] != ' ') {
+        if (BufWord > 0) {
+            CountWord++;
+        }
+        SumChar += BufWord;
+    }
+   return round(static_cast<float>(SumChar) / CountWord);
 }
